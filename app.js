a8c9d08d5b8d7b3967b829181d2b90c83d7ea2f7
reussite <pascal.dlm@yahoo.com>
2024-10-08 23:07:08 +0100

    add cors problems

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var cors = require("cors");
 var usersRouter = require("./routes/users");
 var localitiesRouter = require("./routes/localityRoutes");
 var alertRouter = require("./routes/alertRoutes");
